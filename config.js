a766345f1901f86dd6c579feab22b5f2eaf49094
Olivia Artz <liv@oliviaartz.com>
2022-03-02 12:03:55 -0500

    Removing config file

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    consumer_key:         'aDCwREcFsny6BlGxgVmawBiOR',
-    consumer_secret:      'DkiU0sVtfP6I7HXWWNyTmdxLFudmcTFe2xCRRqHAa8XaoXqDFi',
-    access_token:         '1151941902465458177-Dpe7dTaRN2sgeTHW583yLVJ7JGZO4H',
-    access_token_secret:  'b6W7KEFq3fZ2FZBrf7RKYxSnR46ac1p9alM3utcjHUbZL'
-  }
-  
-  module.exports = config;
